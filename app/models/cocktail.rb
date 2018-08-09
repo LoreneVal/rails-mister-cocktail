beffcccc2cf137f3ab928132bb482c4b8746728e
Lorene Valency <lorene.herzog@gmail.com>
2018-08-09 19:24:59 +0300

    heroku 2 version

--- app/models/cocktail.rb
+++ app/models/cocktail.rb
@@ -2,5 +2,6 @@ class Cocktail < ApplicationRecord
   has_many :doses, dependent: :destroy
   has_many :ingredients, through: :doses
   validates :name, presence: true, uniqueness: true
+  mount_uploader :photo, PhotoUploader
   accepts_nested_attributes_for :doses, reject_if: proc { |c| c.attributes['ingredient_id'].blank?}
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
+  get 'pages/home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :cocktails, only: [:new, :create, :index, :show] do
     resources :doses, only: [:new, :create, :destroy]
   end
-  root to: "cocktails#index"
+  root to: "pages#home"
 end
